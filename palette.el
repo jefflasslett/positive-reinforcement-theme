e846ef2ec065d7011b389969b9c27c88f9231c3f
Jeff Lasslett <jeff@julietlima.net>
2016-02-23 19:35:30 +1100

    Can now use vars in face definitions

--- palette.el
+++ palette.el
@@ -8,14 +8,15 @@
 ;; let s:gray5 = { 'c': '252', 'g': '#d0d0d0' } "
 ;; let s:white = { 'c':  '15', 'g': '#ffffff' } " white
 
-(setq black "#000000"
-      gray0 "#1c1c1c"
-      gary1 "#3a3a3a"
-      gray2 "#585858"
-      gray3 "#808080"
-      gray4 "#b2b2b2"
-      gray5 "#d0d0d0"
-      white "#ffffff")
+(setq pr-black "#000000"
+      pr-gray0 "#1c1c1c"
+      pr-gray1 "#3a3a3a"
+      pr-gray2 "#585858"
+      pr-gray3 "#808080"
+      pr-gray4 "#b2b2b2"
+      pr-gray5 "#d0d0d0"
+      pr-white "#ffffff")
+
 
 
 ;; " blues
@@ -24,10 +25,10 @@
 ;; let s:blue2 = { 'c': '31', 'g': '#0087df' }
 ;; let s:blue3 = { 'c': '45', 'g': '#00dfff' }
 
-(setq blue0 "#0000af"
-      blue1 "#005fdf"
-      blue2 "#0087df"
-      blue3 "#00dfff")
+(setq pr-blue0 "#0000af"
+      pr-blue1 "#005fdf"
+      pr-blue2 "#0087df"
+      pr-blue3 "#00dfff")
 
 
 ;; " red/orange
@@ -36,10 +37,10 @@
 ;; let s:orng2 = { 'c': '184', 'g': '#dfdf00' }
 ;; let s:orng3 = { 'c': '227', 'g': '#ffff5f' }
 
-(setq red0  "#df0000"
-      orng1 "#df8700"
-      orng2 "#dfdf00"
-      orng3 "#ffff5f")
+(setq pr-red0  "#df0000"
+      pr-orng1 "#df8700"
+      pr-orng2 "#dfdf00"
+      pr-orng3 "#ffff5f")
 
 
 ;; " greens
@@ -48,8 +49,9 @@
 ;; let s:grn2 = { 'c': '40', 'g': '#00df00' }
 ;; let s:grn3 = { 'c': '46', 'g': '#00ff00' }
 
-(setq grn0 "#008700"
-      grn1 "#00af00"
-      grn2 "#00df00"
-      grn3 "#00ff00")
+(setq pr-grn0 "#008700"
+      pr-grn1 "#00af00"
+      pr-grn2 "#00df00"
+      pr-grn3 "#00ff00")
 
+(provide 'palette)
